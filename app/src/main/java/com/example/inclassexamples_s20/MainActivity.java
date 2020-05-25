3a06ac9be35681652dc1f72b6d44179a3513fae0
Eric Torunski <etorunski@gmail.com>
2020-05-25 18:41:11 -0400

    creating examples for week 2 code.

--- app/src/main/java/com/example/inclassexamples_s20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_s20/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.inclassexamples_s20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //Load your screen:
         setContentView(R.layout.activity_main);
+
+        ImageButton iBtn = findViewById(R.id.image_Button);
+        iBtn.setOnClickListener( btn -> {  /* code goes here */  });
     }
 }

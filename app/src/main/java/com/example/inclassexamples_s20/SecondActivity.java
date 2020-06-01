1dddd63139fb015d82303ce8ceb7e5773de95869
Eric Torunski <torunse@algonquincollege.com>
2020-06-01 10:58:38 -0400

    changes for week3 slide 8 examples

--- app/src/main/java/com/example/inclassexamples_s20/SecondActivity.java
+++ app/src/main/java/com/example/inclassexamples_s20/SecondActivity.java
@@ -12,11 +12,18 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
+        Intent intent = getIntent(); //This gets you the object nextPage from FirstActivity.java, line 36
+        String name = intent.getStringExtra("name");
+        int age = intent.getIntExtra("age", 0);
+    //    String typed = intent. ??? // what goes here?
+
         Button previousButton = findViewById(R.id.previousPageButton);
 
-        previousButton.setOnClickListener(click ->
-                finish()
-        );
+        previousButton.setOnClickListener(click -> {
+            Intent dataSent = new Intent();
+
+            finish();
+        } );
 
     }
 }
